fdc74f6dcc0ebeafa9ac330f9a0d08568dda8e84
Gleb <gleb.gadbrat@mail.ru>
2022-06-02 17:12:15 +0300

    Задание 3

--- dz.py
+++ dz.py
@@ -14,15 +14,31 @@
 # nok = int(min_common_divisor(a, b))
 # print(f"Наименьшее общее кратное {a} и {b} равно {nok}.")
 
-#Задание 2: Вычислить число Пи c заданной точностью d
-import math
-import decimal
-def get_count(number):
-    s = str(number)
-    if '.' in s:
-        return abs(s.find('.') - len(s)) - 1
-    else:
-        return 0
+# #Задание 2: Вычислить число Пи c заданной точностью d
+# import math
+# import decimal
+# def get_count(number):
+#     s = str(number)
+#     if '.' in s:
+#         return abs(s.find('.') - len(s)) - 1
+#     else:
+#         return 0
 
-d = float(input("Укажите точность: "))
-print(round(math.pi-d, get_count(d)))
+# d = float(input("Укажите точность: "))
+# print(round(math.pi-d, get_count(d)))
+
+#Задание 3 Составить список простых множителей натурального числа N
+a = int(input("Введите число: "))
+prim = []
+def is_primal(chislo):
+    k = 0
+    for i in range(2, chislo//2 + 1):
+        if (chislo % i == 0):
+            k = k+1
+    if (k <= 0):
+        prim.append(chislo)
+for i in range(1,a+1):
+    if a%i==0:
+        is_primal(i)
+print(prim)
+
